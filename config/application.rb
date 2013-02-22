e54fddb403e9f21ceff960825ba094315f80345d
Jordan Goldstein <mrjordangoldstein@gmail.com>
2013-02-22 15:43:19 -0500

    Add and configure devise, add user model through devise

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Ekoschools
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Avoid errors on Heroku
+    config.assets.initialize_on_precompile = false
   end
 end
