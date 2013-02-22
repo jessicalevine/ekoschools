f706cddd7f85fc1c2253450ddd576ec7fb77baa6
Jordan Goldstein <mrjordangoldstein@gmail.com>
2013-02-22 14:09:01 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ekoschools::Application.config.secret_token = '4b3f6662cdc9b33a1908d7af44d6715133ee79e3381f8595a7af9322328bfac4a2e54dc2e52dba974334ae1d6ff865fa1d7d317fddfd1ed57e5faf15a45fcdb6'
